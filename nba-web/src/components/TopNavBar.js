92d0389510f655c5ac2556e29c454d296dd7097c
yideran <ryanjicheng@gmail.com>
2020-05-06 10:31:30 -0700

    nba-web

---
+++ nba-web/src/components/TopNavBar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import logo from "../assets/Images/nba-logoman-word-white.png";
+
+class TopNavBar extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        );
+    }
+}
+
+export default TopNavBar;
